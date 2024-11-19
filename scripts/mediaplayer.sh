fc62bd2dab70dd6912d47bbeed99f25d6b16982a
Xelph <fierrosalinas.la@gmail.com>
2024-11-19 12:56:16 -0700

    Increase maximum album and artist length, fix extra space.

--- scripts/mediaplayer.sh
+++ scripts/mediaplayer.sh
@@ -15,12 +15,12 @@ esac
 
 case $(playerctl metadata album 2> /dev/null) in
     "") # No album name
-        playerctl metadata --format $icon" {{trunc(title, 40)}} - {{trunc(artist, 20)}} "
+        playerctl metadata --format $icon" {{trunc(title, 40)}} - {{trunc(artist, 25)}}"
         ;;
     $(playerctl metadata title)) # Album name equal to song title
-        playerctl metadata --format $icon" {{trunc(title, 40)}} - {{trunc(artist, 20)}} "
+        playerctl metadata --format $icon" {{trunc(title, 40)}} - {{trunc(artist, 25)}}"
         ;;
     *) # All info available
-        playerctl metadata --format $icon" {{trunc(title, 40)}} - {{trunc(artist, 20)}} - {{trunc(album, 20)}}"
+        playerctl metadata --format $icon" {{trunc(title, 40)}} - {{trunc(artist, 25)}} - {{trunc(album, 25)}}"
         ;;
 esac
